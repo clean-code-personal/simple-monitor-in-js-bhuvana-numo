dee0dab029b9f1f4c527010897fdef9ea5ab2d07
bhuvana-numo <bhuvana.y@numocity.com>
2025-02-10 07:23:30 +0530

    Adding testcases to make the test stronger

--- test/bms-monitor-test.js
+++ test/bms-monitor-test.js
@@ -6,7 +6,12 @@ describe('BMS monitor', ()=> {
     expect(batteryIsOk(25, 70, 0.7)).to.be.true;
   });
 
-  it('reports not ok when temperature is out of range', ()=> {
+  it('reports not ok when temperature is high and out of range', ()=> {
     expect(batteryIsOk(50, 85, 0)).to.be.false;
   });
+
+  it('reports not ok when temperature is too low and out of range', () => {
+    expect(batteryIsOk(-5, 70, 0.5)).to.be.false; 
+  });
+  
 });
