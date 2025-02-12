7875f4dabeff31aa76e9692693507466ac646342
bhuvana-numo <bhuvana.y@numocity.com>
2025-02-12 14:13:42 +0530

    extending the code functionality with extension-1 and reducing the length of a line

--- bms-monitor.js
+++ bms-monitor.js
@@ -14,13 +14,12 @@ function isOutOfRange(value, min, max, message) {
 
 function checkingWarnings(value,min,max,tolerance,lowLimMsg,upLimMsg,isEnabled)
 {
- if(!isEnabled) return;
- if(value>=min && value<=min+tolerance){
-   console.log(lowLimMsg);
+ if(isEnabled &&((value>=min && value<=min+tolerance)
+  ||(value>=max-tolerance && value<=max)))
+ {
+  console.log(value <= min + tolerance ? lowLimMsg : upLimMsg);
  }
- if(value>=max-tolerance && value<=max){
-  console.log(upLimMsg);
- }
+
 }
 
 function batteryIsOk(temperature, soc, chargeRate) {
@@ -28,12 +27,20 @@ function batteryIsOk(temperature, soc, chargeRate) {
   const socTolerance=4;
   const chargeRateTolerance=0.04;
 
-  checkingWarnings(temperature,0,45,tempTolerance,'Warning: Approaching lower temperature limit',
-    'Warning: Approaching upper temperature limit',warningSettings.temperature)
-  checkingWarnings(soc,20,80,socTolerance,'Warning: Approaching discharge',
-    'Warning: Approaching charge-peak',warningSettings.soc)
-  checkingWarnings(chargeRate,0,0.8,chargeRateTolerance,'Warning: Approaching lower chargeRate limit',
-    'Warning: Approaching upper chargeRate limit',warningSettings.chargeRate)
+  checkingWarnings(temperature,0,45,tempTolerance,
+    'Warning: Approaching lower temperature limit',
+    'Warning: Approaching upper temperature limit',
+    warningSettings.temperature)
+
+  checkingWarnings(soc,20,80,socTolerance,
+    'Warning: Approaching discharge',
+    'Warning: Approaching charge-peak',
+    warningSettings.soc)
+
+  checkingWarnings(chargeRate,0,0.8,chargeRateTolerance,
+    'Warning: Approaching lower chargeRate limit',
+    'Warning: Approaching upper chargeRate limit',
+    warningSettings.chargeRate)
   
   return !(
     isOutOfRange(temperature, 0, 45, 'Temperature is out of range!') ||
